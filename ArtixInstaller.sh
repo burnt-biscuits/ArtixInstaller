0697763bb823c95b9e4e3b9cdd54b9264eb2374a
Burnt-Biscuits <78766134+burnt-biscuits@users.noreply.github.com>
2024-01-26 15:31:29 -0600

    Update ArtixInstaller.sh

--- ArtixInstaller.sh
+++ ArtixInstaller.sh
@@ -35,6 +35,13 @@ ${normal}
 }
 
 start_install() {
+nc=$(grep -c ^processor /proc/cpuinfo)
+echo "You have " $nc" cores."
+echo "-------------------------------------------------"
+echo "Changing the makeflags for "$nc" cores."
+sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j$nc"/g' /etc/makepkg.conf
+echo "Changing the compression settings for "$nc" cores."
+sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g' /etc/makepkg.conf
     while true; do
         read -srp "Enter root password: " password
         echo
